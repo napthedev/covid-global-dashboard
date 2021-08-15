478a58c0ef6d20444dcb7fc9f797aefadcf5ef3b
NAPTheDev <napthedevhcj@gmail.com>
2021-08-15 14:57:14 +0700

    fetch data successful

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    fetch("https://api.covid19api.com/summary")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    fetch("https://api.covid19api.com/world")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
